c6b9ad3eb104b57878ba7887a23b5b5ce3eea88f
ekatkos <ekat.kos@email.cz>
2026-06-14 18:43:11 +0300

    add: ColumnChart

--- 04-oop-basic-intro-to-ts/1-column-chart/index.ts
+++ 04-oop-basic-intro-to-ts/1-column-chart/index.ts
@@ -1,11 +1,71 @@
 import { createElement } from "../../shared/utils/create-element";
 
 interface Options {
-
+	data?: number[],  
+	label?: string,
+	value?: number,
+	link?: string,
+	formatHeading?: (value: number) => string,
 }
 
 export default class ColumnChart {
-  constructor({ }: Options = {}) {
-    
+	element: HTMLDivElement;
+	chartHeight = 50;
+
+  constructor(private options: Options = {}) {
+    this.render();
+  }
+
+  update(data: number[]): void {
+    this.options.data = data;
+    const body = this.element.querySelector('[data-element="body"]');
+    const maxValue = Math.max(...data);
+    const scale = 50 / maxValue;
+
+    body.innerHTML = data.map(item => `
+      <div style="--value: ${Math.floor(item * scale)}" data-tooltip="${(item / maxValue * 100).toFixed(0)}%"></div>
+    `).join('');
+  }
+
+  remove(): void {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy(): void {
+    this.remove();
+    this.element = null;
+	}
+	
+  private render(): void {
+    this.element = createElement(this.template());
+
+    if (!this.options.data || this.options.data.length === 0) {
+      this.element.classList.add('column-chart_loading');
+    }
+  }
+
+  private template(): string {
+    const maxValue = this.options.data ? Math.max(...this.options.data) : 0;
+	 const scale = this.chartHeight / maxValue;
+	 const heading = this.options.formatHeading ? this.options.formatHeading(this.options.value) : this.options.value
+
+    return `
+      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          ${this.options.label}
+          <a href="${this.options.link}" class="column-chart__link">View all</a>
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${heading}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.options.data?.map(item => `
+              <div style="--value: ${Math.floor(item * scale)}" data-tooltip="${(item / maxValue * 100).toFixed(0)}%"></div>
+            `).join('') ?? ''}
+          </div>
+        </div>
+      </div>
+    `;
   }
 }
